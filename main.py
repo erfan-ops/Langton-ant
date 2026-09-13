9c8930694e9dc10f5e07a75d61a216c4dd198af4
Mohammad Erfan Karami <e.karami@shahrtech.com>
2026-09-13 11:21:04 +0330

    add: ant movement and zoom feature

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import ant_game
 
 
 if __name__ == "__main__":
-    game = ant_game.AntGame(pixel_size=30, grid_line_color=0, background_color=0x262428, ant_color=0xfdfbff)
+    game = ant_game.AntGame(
+        pixel_size=40,
+        width=800,
+        height=800,
+        grid_line_color=0,
+        background_color=0x262428,
+        ant_color=0xc070ff,
+        on_block_color=0xfdfbff
+    )
     game.run()
--- src/ant_game/game.py
+++ src/ant_game/game.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+from typing import Literal
 
 
 _PG_LMB = 1
@@ -7,8 +9,8 @@ class AntGame:
     _window_width: int
     _window_height: int
     
-    _blocks_in_width: int
-    _blocks_in_height: int
+    _blocks_in_width: float
+    _blocks_in_height: float
     
     _block_size: int
     
@@ -17,38 +19,46 @@ class AntGame:
     
     _running: bool = True
     
-    _fps: float = 60
+    _fps: float = 240
     
     _background_color: int
-    
     _grid_line_color: int
+    _on_block_color: int
+    _ant_color: int
     
-    # --- Ant --- #
     # (x, y)
     _ant_pos: list[int, int] = [0, 0]
-    _ant_color: int
+    _ant_direction: Literal[0, 1, 2, 3] = 0 # Up, Right, Down, Left
     
     _camera_offset: list[int, int]
     
-    _on_blocks: set[tuple[int, int]] = {}
+    _on_blocks: set[tuple[int, int]] = set()
+    
+    _min_x_block: int
+    _max_x_block: int
+    _min_y_block: int
+    _max_y_block: int
+    
+    _zoom_factor: float = 1.1
     
     def __init__(
         self,
-        width: int = 20,
-        height: int = 20,
+        width: int = 600,
+        height: int = 600,
         pixel_size: int = 20,
         grid_line_color: int = 0xaba9ad,
         background_color: int = 0x0e0c10,
-        ant_color: int = 0xffffff
-    ):
+        ant_color: int = 0xffffff,
+        on_block_color: int = 0xffffd0
+    ) -> None:
         pygame.init()
         
-        self._blocks_in_width = width
-        self._blocks_in_height = height
-        self._block_size = pixel_size
+        self._window_width  = width
+        self._window_height = height
         
-        self._window_width  = self._blocks_in_width * self._block_size
-        self._window_height = self._blocks_in_height * self._block_size
+        self._block_size = pixel_size
+        self._blocks_in_width = self._window_width / self._block_size
+        self._blocks_in_height = self._window_height / self._block_size
         
         self._screen = pygame.display.set_mode((self._window_width, self._window_height))
         self._clock = pygame.time.Clock()
@@ -63,9 +73,32 @@ class AntGame:
             -int(self._window_width / 2 - self._block_size / 2),
             -int(self._window_height / 2 - self._block_size / 2)
         ]
+        
+        self._min_x_block = int(self._camera_offset[0])
+        self._max_x_block = int(self._camera_offset[0] + self._window_width) + 1
+        
+        self._min_y_block = int(self._camera_offset[1])
+        self._max_y_block = int(self._camera_offset[1] + self._window_height) + 1
+
+        self._on_block_color = on_block_color
     
     
-    def _handle_events(self):
+    def _update_block_size(self, new_size: float):
+        self._block_size = new_size
+        self._blocks_in_width = self._window_width / self._block_size
+        self._blocks_in_height = self._window_height / self._block_size
+    
+    
+    def _zoom_in(self, zoom: float):
+        zoom_amount = self._zoom_factor ** zoom
+        self._update_block_size(self._block_size * zoom_amount)
+    
+    def _zoom_out(self, zoom: float):
+        zoom_amount = self._zoom_factor ** zoom
+        self._update_block_size(self._block_size / zoom_amount)
+    
+    
+    def _handle_events(self) -> None:
         for event in pygame.event.get():
             # Window events
             if event.type == pygame.QUIT:
@@ -83,43 +116,98 @@ class AntGame:
                     # Move camera
                     self._camera_offset[0] -= event.rel[0]
                     self._camera_offset[1] -= event.rel[1]
+                    
+                    self._min_x_block = int(self._camera_offset[0])
+                    self._max_x_block = int(self._camera_offset[0] + self._window_width) + 1
+                    
+                    self._min_y_block = int(self._camera_offset[1])
+                    self._max_y_block = int(self._camera_offset[1] + self._window_height) + 1
+            elif event.type == pygame.MOUSEWHEEL:
+                scroll_value = event.precise_y
+                if scroll_value > 0:
+                    self._zoom_in(scroll_value)
+                else:
+                    self._zoom_out(abs(scroll_value))
 
 
-    def _draw_grid_lines(self):
+    def _draw_grid_lines(self) -> None:
         # vertical lines
-        for x_factor in range(1, self._blocks_in_width + 1):
+        for x_factor in range(1, int(self._blocks_in_width + 1)):
             x = x_factor * self._block_size - self._camera_offset[0] % self._block_size
             pygame.draw.aaline(self._screen, self._grid_line_color, (x, 0), (x, self._window_height))
         
         # horizontal lines
-        for y_factor in range(1, self._blocks_in_width + 1):
+        for y_factor in range(1, int(self._blocks_in_height + 1)):
             y = y_factor * self._block_size - self._camera_offset[1] % self._block_size
             pygame.draw.aaline(self._screen, self._grid_line_color, (0, y), (self._window_width, y))
 
 
-    def _draw_ant(self):
+    def _draw_ant(self) -> None:
         pygame.draw.rect(
             self._screen,
             self._ant_color,
             (
                 self._ant_pos[0] * self._block_size - self._camera_offset[0],
                 self._ant_pos[1] * self._block_size - self._camera_offset[1],
-                self._block_size,
-                self._block_size
+                math.ceil(self._block_size),
+                math.ceil(self._block_size)
             )
         )
     
     
-    def run(self):
+    def _is_on(self, xy: tuple[int, int]) -> bool:
+        return xy in self._on_blocks
+    
+    
+    def _draw_blocks(self) -> None:
+        for x_block in range(self._min_x_block, self._max_x_block):
+            for y_block in range(self._min_y_block, self._max_y_block):
+                if self._is_on((x_block, y_block)):
+                    x = x_block * self._block_size - self._camera_offset[0]
+                    y = y_block * self._block_size - self._camera_offset[1]
+                    pygame.draw.rect(self._screen, self._on_block_color, (x, y, math.ceil(self._block_size), math.ceil(self._block_size)))
+    
+    
+    def _invert_block(self, xy: tuple[int, int]) -> None:
+        if self._is_on(xy):
+            self._on_blocks.remove(xy)
+        else:
+            self._on_blocks.add(xy)
+
+    
+    def _ant_forward(self):
+        if (self._ant_direction == 0):
+            self._ant_pos[1] += 1
+        elif (self._ant_direction == 1):
+            self._ant_pos[0] += 1
+        elif (self._ant_direction == 2):
+            self._ant_pos[1] -= 1
+        else:
+            self._ant_pos[0] -= 1
+    
+    
+    def _ant_step(self):
+        if self._is_on(tuple(self._ant_pos)):
+            self._ant_direction = (self._ant_direction + 1) % 4
+        else:
+            self._ant_direction = (self._ant_direction + 3) % 4
+        
+        self._invert_block(tuple(self._ant_pos))
+        self._ant_forward()
+    
+    
+    def run(self) -> None:
         while self._running:
             self._handle_events()
             
             # fill the background
             self._screen.fill(self._background_color)
             
-            self._draw_grid_lines()
-            
+            self._draw_blocks()
             self._draw_ant()
+            # self._draw_grid_lines()
+            
+            self._ant_step()
             
             
             pygame.display.flip()
